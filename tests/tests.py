34dec2b690b42dbee518c9ce1c1284863d5f3fe6
Lyndsy Simon <lyndsy@lyndsysimon.com>
2016-07-05 16:03:23 -0400

    Add another example test

--- tests/tests.py
+++ tests/tests.py
@@ -4,3 +4,6 @@ from django.test import TestCase
 class ExampleTestCase(TestCase):
     def test_nothing(self):
         self.assertEqual(1, 1)
+
+    def test_another_nothing(self):
+        self.assertEqual(2, 2)
