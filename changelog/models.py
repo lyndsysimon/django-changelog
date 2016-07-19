cfe09097e94b2deeacda5577580b0ab9420259bc
Lyndsy Simon <lyndsy@lyndsysimon.com>
2016-07-18 20:29:37 -0400

    Add initial implementation of

--- changelog/models.py
+++ changelog/models.py
@@ -34,3 +34,59 @@ class ChangeLog(models.Model):
             model_name=self.instance._meta.label,
             instance_id=self.object_id,
         )
+
+
+class ChangeSet(object):
+    def __init__(self, first=None, last=None, instance=None):
+        """A summary of changes to tracked fields between two states.
+
+        Any combination of parameters may be passed, provided they refer to
+        the same model instance.
+
+        If only ``instance`` is passed, then all ``ChangeLogs`` will be
+        included.
+
+        If only ``first`` is passed, then all ``ChangeLog``s after will be
+        included. Likewise, if only ``last`` is passed, then all
+        ``ChangeLog``s prior will be included.
+
+        :param first:
+        :param last:
+        :param instance:
+        """
+
+        if not any((first, last, instance)):
+            raise TypeError("At least one keyword argument must be provided.")
+
+        # ensure all params refer to the same instance
+
+        if instance is not None:
+            if first is not None:
+                assert first.instance.pk == instance.pk
+            if last is not None:
+                assert last.instance.pk == instance.pk
+
+        if first is not None:
+            if last is not None:
+                assert last.instance.pk == first.instance.pk
+
+        # set instance variables
+
+        self.instance = instance
+        self.first = first
+        self.last = last
+
+        if self.instance is None:
+            self.instance = (self.first or self.last).instance
+
+        if self.first is None:
+            self.first = ChangeLog.objects.filter(
+                content_type=ContentType.objects.get_for_model(self.instance),
+                object_id=self.instance.pk,
+            ).order_by('created_at').first()
+
+        if self.last is None:
+            self.last = ChangeLog.objects.filter(
+                content_type=ContentType.objects.get_for_model(self.instance),
+                object_id=self.instance.pk,
+            ).order_by('-created_at').first()
--- tests/tests.py
+++ tests/tests.py
@@ -1,14 +1,15 @@
 from __future__ import absolute_import
 
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 
 from . import factories
 from .models import TrackedModel, UntrackedModel
-from changelog.models import ChangeLog
+from changelog.models import ChangeLog, ChangeSet
 
 
-class ChangelogTestCase(TestCase):
+class ChangeLogTestCase(TestCase):
     def setUp(self):
         pass
 
@@ -72,3 +73,121 @@ class ChangelogTestCase(TestCase):
                 }
             }
         )
+
+class ChangeSetTestCase(TestCase):
+    def setUp(self):
+        self.tracked = factories.TrackedModelFactory()
+        self.tracked.tracked_char = 'first value'
+        self.tracked.save()
+        self.tracked.tracked_char = 'second value'
+        self.tracked.save()
+        self.tracked.tracked_char = 'third value'
+        self.tracked.save()
+        self.tracked.tracked_char = 'fourth value'
+        self.tracked.save()
+        self.tracked.tracked_char = 'fifth value'
+        self.tracked.save()
+
+    def tearDown(self):
+        TrackedModel.objects.all().delete()
+
+    def test_init_instance_param(self):
+        x = ChangeSet(instance=self.tracked)
+
+        self.assertEqual(
+            self.tracked.pk,
+            x.instance.pk,
+        )
+
+    def test_init_first_param(self):
+        logs = list(
+            ChangeLog.objects.filter(
+                content_type=ContentType.objects.get_for_model(self.tracked),
+                object_id=self.tracked.pk,
+            ).order_by('created_at').all()
+        )
+        x = ChangeSet(first=logs[2])
+
+        self.assertEqual(
+            logs[2].pk,
+            x.first.pk,
+        )
+
+        self.assertEqual(
+            logs[-1].pk,
+            x.last.pk,
+        )
+
+        self.assertEqual(
+            self.tracked,
+            x.instance,
+        )
+
+    def test_init_last_param(self):
+        logs = list(
+            ChangeLog.objects.filter(
+                content_type=ContentType.objects.get_for_model(self.tracked),
+                object_id=self.tracked.pk,
+            ).order_by('created_at').all()
+        )
+        x = ChangeSet(last=logs[2])
+
+        self.assertEqual(
+            logs[2].pk,
+            x.last.pk,
+        )
+
+        self.assertEqual(
+            logs[0].pk,
+            x.first.pk,
+        )
+
+        self.assertEqual(
+            self.tracked,
+            x.instance,
+        )
+
+    def test_init_param_mismatch(self):
+        logs = list(
+            ChangeLog.objects.filter(
+                content_type=ContentType.objects.get_for_model(self.tracked),
+                object_id=self.tracked.pk,
+            ).order_by('created_at').all()
+        )
+
+        other = factories.TrackedModelFactory()
+        other.tracked_char = 'first value'
+        other.save()
+        other.tracked_char = 'second value'
+        other.save()
+
+        other_logs = list(
+            ChangeLog.objects.filter(
+                content_type=ContentType.objects.get_for_model(other),
+                object_id=other.pk,
+            ).order_by('created_at').all()
+        )
+
+        # instance param is from another instance
+        with self.assertRaises(AssertionError):
+            ChangeSet(
+                instance=other,
+                first=logs[0],
+                last=logs[-1],
+            )
+
+        # first param is from another instance
+        with self.assertRaises(AssertionError):
+            ChangeSet(
+                instance=self.tracked,
+                first=other_logs[0],
+                last=logs[-1],
+            )
+
+        # last param is from another instance
+        with self.assertRaises(AssertionError):
+            ChangeSet(
+                instance=self.tracked,
+                first=logs[0],
+                last=other_logs[0],
+            )
