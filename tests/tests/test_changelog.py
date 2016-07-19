09067a09ecc325567c99df47fc7cee669ee4e5c6
Lyndsy Simon <lyndsy@lyndsysimon.com>
2016-07-19 10:22:23 -0400

    Refactor tests into separate files

---
+++ tests/tests/test_changelog.py
@@ -0,0 +1,75 @@
+from __future__ import absolute_import
+
+from django.test import TestCase
+
+from tests import factories
+from tests.models import TrackedModel, UntrackedModel
+from changelog.models import ChangeLog
+
+
+class ChangeLogTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        TrackedModel.objects.all().delete()
+        UntrackedModel.objects.all().delete()
+
+    def test_no_log_for_initial_creation(self):
+        factories.TrackedModelFactory()
+
+        self.assertEqual(
+            0,
+            ChangeLog.objects.count()
+        )
+
+    def test_tracked_field_logged(self):
+        x = factories.TrackedModelFactory()
+        x.tracked_char = 'asdf'
+        x.save()
+
+        self.assertEqual(
+            1,
+            ChangeLog.objects.count()
+        )
+
+    def test_char_field_was_none(self):
+        x = factories.TrackedModelFactory(tracked_char=None)
+        x.tracked_char = 'new value'
+        x.save()
+
+        log = ChangeLog.objects.get()
+
+        self.assertIsNone(
+            log.fields['tracked_char']['was']
+        )
+
+    def test_char_field_now_none(self):
+        x = factories.TrackedModelFactory()
+        x.tracked_char = None
+        x.save()
+
+        log = ChangeLog.objects.get()
+
+        self.assertIsNone(
+            log.fields['tracked_char']['now']
+        )
+
+    def test_char_field_correct(self):
+        x = factories.TrackedModelFactory(
+            tracked_char='initial value'
+        )
+        x.tracked_char = 'new value'
+        x.save()
+
+        log = ChangeLog.objects.get()
+
+        self.assertEqual(
+            log.fields,
+            {
+                'tracked_char': {
+                    'was': 'initial value',
+                    'now': 'new value',
+                }
+            }
+        )
--- tests/tests/test_changeset.py
+++ tests/tests/test_changeset.py
@@ -1,78 +1,12 @@
 from __future__ import absolute_import
 
-from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 
-from . import factories
-from .models import TrackedModel, UntrackedModel
+from tests import factories
+from tests.models import TrackedModel
 from changelog.models import ChangeLog, ChangeSet
 
-
-class ChangeLogTestCase(TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        TrackedModel.objects.all().delete()
-        UntrackedModel.objects.all().delete()
-
-    def test_no_log_for_initial_creation(self):
-        factories.TrackedModelFactory()
-
-        self.assertEqual(
-            0,
-            ChangeLog.objects.count()
-        )
-
-    def test_tracked_field_logged(self):
-        x = factories.TrackedModelFactory()
-        x.tracked_char = 'asdf'
-        x.save()
-
-        self.assertEqual(
-            1,
-            ChangeLog.objects.count()
-        )
-
-    def test_char_field_was_none(self):
-        x = factories.TrackedModelFactory(tracked_char=None)
-        x.tracked_char = 'new value'
-        x.save()
-
-        log = ChangeLog.objects.get()
-
-        self.assertIsNone(
-            log.fields['tracked_char']['was']
-        )
-
-    def test_char_field_now_none(self):
-        x = factories.TrackedModelFactory()
-        x.tracked_char = None
-        x.save()
-
-        log = ChangeLog.objects.get()
-
-        self.assertIsNone(
-            log.fields['tracked_char']['now']
-        )
-
-    def test_char_field_correct(self):
-        x = factories.TrackedModelFactory()
-        x.tracked_char = 'new value'
-        x.save()
-
-        log = ChangeLog.objects.get()
-
-        self.assertEqual(
-            log.fields,
-            {
-                'tracked_char': {
-                    'was': 'value-0',
-                    'now': 'new value',
-                }
-            }
-        )
 
 class ChangeSetTestCase(TestCase):
     def setUp(self):
